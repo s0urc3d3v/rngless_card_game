4145776de0c4d5c226edd6403cbf0acc9aef7f3a
matthewelbing <author@techredesign.net>
2017-02-06 13:01:02 -0500

    remove german

--- core/src/com/mygdx/game/game.java
+++ core/src/com/mygdx/game/game.java
@@ -19,7 +19,6 @@ public class game extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		//gemacht die dingen heir mit bild!
 		batch.end();
 	}
 	
