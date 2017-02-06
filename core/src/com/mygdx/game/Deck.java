fd0e464e80d5633f8c1debaab2820636fc2f6ee6
matthewelbing <author@techredesign.net>
2017-02-06 13:09:35 -0500

    add view

--- core/src/com/mygdx/game/Deck.java
+++ core/src/com/mygdx/game/Deck.java
@@ -3,5 +3,5 @@ package com.mygdx.game;
 /**
  * Created by matthewelbing on 06.02.17.
  */
-public class Deck {
+public class Deck { //Here, we should have a array of cards in this class
 }
---
+++ core/src/com/mygdx/game/Views/View.java
@@ -0,0 +1,7 @@
+package com.mygdx.game.Views;
+
+/**
+ * Created by matthewelbing on 06.02.17.
+ */
+public abstract class View { //update, etc
+}
