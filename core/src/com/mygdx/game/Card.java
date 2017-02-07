bb3a9a396007e48e890bc21c73b0bf1fe8ec6763
tzur_almog <tzur.almog@gmail.com>
2017-02-07 10:54:40 -0500

    deleted stats, discoverd it was useless

--- core/src/com/mygdx/game/Card.java
+++ core/src/com/mygdx/game/Card.java
@@ -4,4 +4,5 @@ package com.mygdx.game;
  * Created by matthewelbing on 06.02.17.
  */
 public class Card { //This class should be implementing a "Cards" interface
+
 }
--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -4,4 +4,6 @@ package com.mygdx.game;
  * Created by matthewelbing on 06.02.17.
  */
 public class Player {
+
+    public int Health;
 }
