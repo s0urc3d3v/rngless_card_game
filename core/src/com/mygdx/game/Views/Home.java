da340702bc7b9d8daba98a4fb7614e3c5e51e066
matthewelbing <author@techredesign.net>
2017-02-10 13:17:42 -0500

    add logic loop

--- core/src/com/mygdx/game/Views/Home.java
+++ core/src/com/mygdx/game/Views/Home.java
@@ -1,10 +1,16 @@
 package com.mygdx.game.Views;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.mygdx.game.Game;
 
+
 public class Home extends View {
     ShapeRenderer renderer = new ShapeRenderer();
+    SpriteBatch spriteBatchMenu = new SpriteBatch();
+    BitmapFont title = new BitmapFont();
     @Override
     public void render() {
 
