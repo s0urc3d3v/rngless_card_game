dcd2dc69507e9a1f55f65cdfa8010f00e53a0439
john_bachman <jack.github@gmail.com>
2017-03-28 13:56:52 -0400

    Importing DB_Tool

--- core/src/com/mygdx/game/Views/Board.java
+++ core/src/com/mygdx/game/Views/Board.java
@@ -14,6 +14,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.mygdx.game.Cards.Card;
 import com.mygdx.game.Cards.Minion;
+import com.mygdx.game.Core.DB_tool;
 import com.mygdx.game.Game;
 import com.mygdx.game.UI.SimpleButton;
 
