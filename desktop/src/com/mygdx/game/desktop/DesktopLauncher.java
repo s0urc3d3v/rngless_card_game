80852a9d1de069ef7e4898dc3b05cd7870a2deb1
john_bachman <jack.github@gmail.com>
2017-04-11 11:41:01 -0400

    Error with getting texture using sandworm

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,13 +2,14 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import com.mygdx.game.Game;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		//TexturePacker.process("raw_textures/buttons", "packed_textures/buttons", "buttons");
+//		TexturePacker.process("raw_textures/cards", "packed_textures/cards", "allcards");
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.fullscreen = true;
+//		config.fullscreen = true;
 		config.resizable = false;
 		new LwjglApplication(new Game(), config);
 	}
