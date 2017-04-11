3791bfe3023c5cdf1b76c6d689e5ea5a0b98211f
matthewelbing <author@techredesign.net>
2017-04-11 11:41:09 -0400

    fix broken library

--- core/src/com/mygdx/game/Views/CardSelector.java
+++ core/src/com/mygdx/game/Views/CardSelector.java
@@ -2,18 +2,36 @@ package com.mygdx.game.Views;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.scenes.scene2d.ui.TextArea;
 import com.mygdx.game.Game;
 
 public class CardSelector extends View implements ViewSwitchListener {
+    private SpriteBatch spriteBatch = new SpriteBatch();
+    private Camera camera;
+
 
     @Override
     public void render() {
+        refresh();
         Gdx.gl.glClearColor(100 / 255f, 100/ 255f, 100 / 255f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)){
             returnIndex = Game.viewIndexes.HOME;
         }
+        spriteBatch.begin();
+
+        BitmapFont font = new BitmapFont();
+        font.setColor(new Color(105f/255f, 0, 248f/255f, 1));
+        font.draw(spriteBatch, "All Cards", Gdx.graphics.getHeight(), Gdx.graphics.getWidth()/2);
+        spriteBatch.end();
+
     }
 
     @Override
@@ -30,6 +48,15 @@ public class CardSelector extends View implements ViewSwitchListener {
     @Override
     public void create() {
         super.create();
+        camera = stage.getCamera();
+
+    }
+
+    private void refresh(){
+        stage.getCamera().update();
+        spriteBatch.setProjectionMatrix(camera.combined);
+        spriteBatch.getProjectionMatrix().setToOrtho2D(0,0,stage.getWidth(), stage.getWidth());
+
     }
 
 
