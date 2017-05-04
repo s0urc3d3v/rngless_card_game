a30326f2d318739b334bc444b89665b86e0dd8e6
matthewelbing <author@techredesign.net>
2017-05-04 13:13:44 -0400

    add script

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,11 +2,12 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import com.mygdx.game.Game;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-//		TexturePacker.process("raw_textures/cards", "packed_textures/cards", "allcards");
+		//TexturePacker.process("raw_textures/cards", "packed_textures/cards", "allcards");
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.resizable = false;
 		config.width = LwjglApplicationConfiguration.getDesktopDisplayMode().width;
